0ddf53c313451f429aab5938bb30882d8ad4db93
Matt DSena <matthewdsena@gmail.com>
2022-01-07 15:08:11 +1030

    had const instead of let for my shuffled questions and thats why wasnt working

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElements = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionsIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame () {
 console.log('Started')
 startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionsIndex = 0
 questionContainerElement.classList.remove('hide')
 setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionsIndex])
 
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
